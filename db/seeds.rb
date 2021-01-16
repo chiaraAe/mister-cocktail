ea2564dbf37f4bd765bfd28bee78a91cc04f808d
Chiara Aengevelt <chiara@aengevelt.com>
2021-01-16 17:11:28 +0100

    created three models and added 2 controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clear database of Ingredients"
+Ingredient.destroy_all
+Cocktail.destroy_all
+puts "Database cleared"
+
+5.times do 
+  ingredient = Ingredient.create(
+    name: Faker::Food.ingredient
+  )
+  puts "ingredient #{ingredient.id} created"
+end 
+
+5.times do 
+  cocktail = Cocktail.create(
+    name: Faker::Cannabis.brand
+  )
+  puts "cocktail #{cocktail.id} created" 
+end
+
+
+
+# #cocktail name
+# Faker::Cannabis.brand
+
+# # Doses
+# Faker::Food.measurement
+# Faker::Food.description
+
+# # ingredience name
+# Faker::Food.ingredient
